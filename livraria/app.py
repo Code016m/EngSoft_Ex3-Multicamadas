c9c48d11e354fba3c05ef1a7b9d6a6a5ea86aaca
Code016m <miguel.r.araujo16@gmail.com>
2026-04-27 18:00:02 -0300

    Refactor app.py to use executar_app function for better structure

--- livraria/app.py
+++ livraria/app.py
@@ -1,4 +1,10 @@
 from livraria.application.livro_controller import LivroController
 
+
+def executar_app():
+    controller = LivroController()
+    controller.iniciar()
+
+
 if __name__ == "__main__":
-    LivroController().iniciar()
+    executar_app()
