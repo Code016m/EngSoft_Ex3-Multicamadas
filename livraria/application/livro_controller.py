714d79c3914341316ca297ea9234b01b66bc85e4
Miguel R. Araújo <143842829+Code016m@users.noreply.github.com>
2026-04-27 17:48:24 -0300

    Implement LivroController with CRUD operations

--- livraria/application/livro_controller.py
+++ livraria/application/livro_controller.py
@@ -1,1 +1,34 @@
+from livraria.presentation.livro_view import LivroView
+from livraria.domain.livro_service import LivroService
 
+class LivroController:
+
+    def __init__(self):
+        self.service = LivroService()
+        self.view = LivroView()
+        self.view.set_controller(self)
+
+    def cadastrar_livro(self):
+        livro = self.view.obter_dados()
+        self.service.cadastrar_livro(livro)
+        self.view.limpar_campos()
+        self.listar_livros()
+
+    def listar_livros(self):
+        livros = self.service.listar_livros()
+        self.view.mostrar_livros(livros)
+
+    def excluir_livro(self):
+        indice = self.view.obter_indice_selecionado()
+
+        if indice is not None:
+            self.service.excluir_livro(indice)
+            self.listar_livros()
+
+    def buscar_livro(self):
+        texto = self.view.obter_busca()
+        livros = self.service.buscar_por_titulo(texto)
+        self.view.mostrar_livros(livros)
+
+    def iniciar(self):
+        self.view.iniciar()
