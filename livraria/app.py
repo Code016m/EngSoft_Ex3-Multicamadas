723f26bc6b3da57ba7c522e73331bae299bf43fd
Miguel R. Araújo <143842829+Code016m@users.noreply.github.com>
2026-04-27 17:46:30 -0300

    Update import path for LivroController

--- livraria/app.py
+++ livraria/app.py
@@ -1,4 +1,4 @@
-from livraria.controller.livro_controller import LivroController
+from livraria.application.livro_controller import LivroController
 
 if __name__ == "__main__":
     LivroController().iniciar()
