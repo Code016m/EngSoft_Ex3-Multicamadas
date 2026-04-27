29c21b6ed32a6731c9ee94e604cc322dc2df8429
Miguel R. Araújo <143842829+Code016m@users.noreply.github.com>
2026-04-27 17:47:32 -0300

    Implement LivroRepository with CRUD methods

--- livraria/infrastructure/livro_repository.py
+++ livraria/infrastructure/livro_repository.py
@@ -1,1 +1,17 @@
+class LivroRepository:
 
+    def __init__(self):
+        self.livros = [
+            {"titulo": "Livro A", "autor": "Autor 1", "categoria": "Ficção", "preco": 30},
+            {"titulo": "Livro B", "autor": "Autor 2", "categoria": "Técnico", "preco": 50},
+            {"titulo": "Livro C", "autor": "Autor 3", "categoria": "Drama", "preco": 40},
+        ]
+
+    def listar(self):
+        return self.livros
+
+    def adicionar(self, livro):
+        self.livros.append(livro)
+
+    def excluir(self, indice):
+        del self.livros[indice]
