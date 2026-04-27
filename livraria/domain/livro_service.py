dfeaaedfef1f542368a5cb67f17f039ee4fcd0ad
Miguel R. Araújo <143842829+Code016m@users.noreply.github.com>
2026-04-27 17:48:04 -0300

    Implement LivroService with CRUD operations

--- livraria/domain/livro_service.py
+++ livraria/domain/livro_service.py
@@ -1,1 +1,23 @@
+from livraria.infrastructure.livro_repository import LivroRepository
 
+class LivroService:
+
+    def __init__(self):
+        self.repo = LivroRepository()
+
+    def listar_livros(self):
+        return self.repo.listar()
+
+    def cadastrar_livro(self, livro):
+        self.repo.adicionar(livro)
+
+    def excluir_livro(self, indice):
+        self.repo.excluir(indice)
+
+    def buscar_por_titulo(self, texto):
+        livros = self.repo.listar()
+
+        return [
+            livro for livro in livros
+            if texto.lower() in livro["titulo"].lower()
+        ]
