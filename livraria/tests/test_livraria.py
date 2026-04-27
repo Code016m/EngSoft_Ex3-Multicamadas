f367894693aae970f3d089c2d6ebc3d30f6e6077
Code016m <miguel.r.araujo16@gmail.com>
2026-04-27 17:57:50 -0300

    New test

--- livraria/tests/test_livraria.py
+++ livraria/tests/test_livraria.py
@@ -1,31 +1,39 @@
 import unittest
-from livraria.model.livro_model import LivroModel
+from livraria.domain.livro_service import LivroService
 
 
-class TestLivrariaMVC(unittest.TestCase):
+class TestLivrariaMulticamadas(unittest.TestCase):
 
     def test_listar_livros(self):
-        model = LivroModel()
-        self.assertEqual(len(model.listar_livros()), 3)
+        service = LivroService()
 
-    def test_adicionar_livro(self):
-        model = LivroModel()
+        self.assertEqual(len(service.listar_livros()), 3)
 
-        model.adicionar_livro({
+    def test_cadastrar_livro(self):
+        service = LivroService()
+
+        service.cadastrar_livro({
             "titulo": "Novo",
             "autor": "Autor X",
             "categoria": "Drama",
             "preco": 45
         })
 
-        self.assertEqual(len(model.listar_livros()), 4)
+        self.assertEqual(len(service.listar_livros()), 4)
 
     def test_excluir_livro(self):
-        model = LivroModel()
+        service = LivroService()
 
-        model.excluir_livro(0)
+        service.excluir_livro(0)
 
-        self.assertEqual(len(model.listar_livros()), 2)
+        self.assertEqual(len(service.listar_livros()), 2)
+
+    def test_buscar_livro(self):
+        service = LivroService()
+
+        resultado = service.buscar_por_titulo("Livro A")
+
+        self.assertEqual(len(resultado), 1)
 
 
 if __name__ == "__main__":
